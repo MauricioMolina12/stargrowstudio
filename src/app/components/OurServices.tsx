d68edcb1639ec932ef9640ae7ba4fad0c896d7d9
Mauricio Molina <molinamau37@gmail.com>
2025-07-15 16:44:56 -0500

    fixing details for deploy in vercel

--- src/app/components/OurServices.tsx
+++ src/app/components/OurServices.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useEffect, useState } from "react";
-import Image from "next/image";
+import { useState } from "react";
 import clsx from "clsx";
 import useDarkMode from "../hooks/useDarkMode";
 import { useIntersectionObserver } from "../hooks/useIntersectionObserver";

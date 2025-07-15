5182a97eb402aeb3fbfa1e1e0b7794e4d9e34197
Mauricio Molina <molinamau37@gmail.com>
2025-07-15 16:54:57 -0500

    fixing details in page.tsx

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -1,22 +1,16 @@
 import { services } from "@/app/data/services";
 import Banner from "@/app/components/shared/Banner";
 
-type Props = {
-  params: {
-    slug: string;
-  };
-};
-
-export default function ServiceSlugPage({ params }: Props) {
+export default function ServiceSlugPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const service = services.find((s) => s.slug === params.slug);
 
   if (!service) {
     return <h1>Servicio no encontrado</h1>;
   }
 
-  return (
-    <>
-      <Banner banners={[service]} />
-    </>
-  );
+  return <Banner banners={[service]} />;
 }
